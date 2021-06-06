9abe760280034368fdf567be48a96002a6164d2d
Efrain Hernandez <efrainhg2050@gmail.com>
2021-06-05 20:34:00 -0500

    Test on gold and silver button

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,1 +1,6 @@
-alert('Todo Ok');
+const goldButton = document.getElementById('gold');
+const silverButton = document.getElementById('silver');
+
+
+goldButton.addEventListener("click", () =>  console.log("Gold"));
+silverButton.addEventListener("click", () => console.log('Silver'));
