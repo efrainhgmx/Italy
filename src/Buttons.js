207f5b9db96dfba5bd9787e4d5e8928e4bc6867d
Efrain Hernandez <efrainhg2050@gmail.com>
2021-06-05 20:45:44 -0500

    source changed on product image

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,5 +1,14 @@
 const goldButton = document.getElementById('gold');
 const silverButton = document.getElementById('silver');
+const productImage = document.getElementById('product-image');
+const silverImage = 'https://efrainhgmx.com/Italy/assets/images/stainless-steel.png';
+const goldImage = 'https://efrainhgmx.com/Italy/assets/images/feedback.png';
+
+const goldColor = () => {
+    productImage.src === silverImage ? console.log('Es igual') : console.log('No es igual')
+}
+
+
 
 
 goldButton.addEventListener("click", () =>  console.log("Gold"));
